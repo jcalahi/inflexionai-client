309914b8e82977c03cc8f489e3b2189d5e0297ba
jcalahi <juliuscalahi@gmail.com>
2023-08-13 12:42:12 +0800

    added server endpoint

--- src/apis.js
+++ src/apis.js
@@ -1,25 +1,27 @@
 import axios from "axios";
 
+const SERVER_URL = "https://inflexioai-api-production.up.railway.app";
+
 export const uploadAudio = async (blob) => {
   const formData = new FormData();
   formData.append("recording", blob);
   const {
     data: { filename },
-  } = await axios.post("/api/fileupload", formData);
+  } = await axios.post(`${SERVER_URL}/api/fileupload`, formData);
   return filename;
 };
 
 export const transcribeFileContent = async (fileName) => {
-  const data = await axios.post("/api/transcribe", { fileName });
+  const data = await axios.post(`${SERVER_URL}/api/transcribe`, { fileName });
   return data;
 };
 
 export const summarizeText = async (text) => {
-  const data = await axios.post("/api/summary", { text });
+  const data = await axios.post(`${SERVER_URL}/api/summary`, { text });
   return data;
 };
 
 export const fetchTestimonials = async () => {
-  const data = await axios.get("/api/testimonials");
+  const data = await axios.get(`${SERVER_URL}/api/testimonials`);
   return data;
 };
